f2dc7a6708245fd899f0a3af5972e19945906918
IT21223976 <it21223976@my.sliit.lk>
2024-05-03 00:27:13 +0530

    Created Research Paper Publication form

--- frontend/src/pages/student/StdRePaperPublication.jsx
+++ frontend/src/pages/student/StdRePaperPublication.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import StdHeader from '../../components/student/header/StdHeader'
+import StdHeader from '../../components/student/header/StdHeader';
+import '../../styles/student/StdRePaperPublication.css';
 
 function StdRePaperPublication ()
 {
@@ -10,13 +11,13 @@ function StdRePaperPublication ()
             <div className='std-grp-reg-container-inner'>
                 <form className='std-grp-reg-from'>
                     <div className='std-grp-reg-form-inner'>
-                        <div className='std-grp-reg-from-inner-section1'>
-                            <div className='main-heading'>
-                                <span className='heading-blue'>Research</span>
-                                <span className='heading-orange'> Paper</span>
-                                <span className='heading-blue'> Publication</span>
-                            </div>
+                        <div className='main-heading'>
+                            <span className='heading-blue'>Research</span>
+                            <span className='heading-orange'> Paper</span>
+                            <span className='heading-blue'> Publication</span>
+                        </div>
 
+                        <div className='std-grp-reg-from-inner-section1'>
                             <div className='std-grp-reg-form-inner-sec1-inner'>
                                 <div className='std-grp-reg-form-field'>
                                     <label className='std-grp-reg-form-label' htmlFor='title'>Title of the research paper  :</label>
@@ -76,7 +77,7 @@ function StdRePaperPublication ()
                                     <label className='std-grp-reg-form-label' htmlFor='supervisor'>Supervisor  :</label>
 
                                     <select
-                                        className='std-grp-reg-from-select-student'
+                                        className='std-grp-reg-from-select-supervisor'
                                         name='supervisor'
                                     >
                                         <option>Supervisor</option>
@@ -90,7 +91,7 @@ function StdRePaperPublication ()
                                     <label className='std-grp-reg-form-label' htmlFor='cosupervisor'>Co-Supervisor  :</label>
 
                                     <select
-                                        className='std-grp-reg-from-select-student'
+                                        className='std-grp-reg-from-select-cosupervisor'
                                         name='cosupervisor'
                                     >
                                         <option>Co-Supervisor</option>
